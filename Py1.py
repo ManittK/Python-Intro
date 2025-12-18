9dbe514ac636480a7476d4f46884f878481edc56
ManittK <manitt.kedia@gmail.com>
2025-12-18 20:59:32 +0530

    py1

---
+++ Py1.py
@@ -0,0 +1,8 @@
+#Printing a greeting message
+print("Hello World!")
+#Printing my name
+print("My name is Manitt.")
+#Fun Fact about yourself
+print("I like to code and make stuff with robotics")
+#Performing a simple calculation
+print("The product of 3 and 5 is", 3*5)
