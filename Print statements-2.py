dd7c12f01faa6843b1017fc9d150986883ad5a65
ManittK <manitt.kedia@gmail.com>
2025-12-23 20:16:18 +0530

    py2

---
+++ Print statements-2.py
@@ -0,0 +1,10 @@
+#Welcome to the coding world
+print("Welcome to the world of coding!")
+#Entering the number 4 as the print
+print(4)
+#Next line in Python
+print("Hello World \n")
+#Managing multiple arguements at once
+print("Hello to world", 616)
+#End arguement of print statement
+print("Welcome to the end-", end="*")
