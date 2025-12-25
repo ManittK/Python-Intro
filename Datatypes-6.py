b62192f7ddebb9d5583a8d1a88116578310a1dde
ManittK <manitt.kedia@gmail.com>
2025-12-25 20:08:36 +0530

    py6

---
+++ Datatypes-6.py
@@ -0,0 +1,14 @@
+a = 5
+print("The given value is type/class:", type(a))
+
+b = 26.11
+print("The given value is type/class:", type(b))
+
+c = "Manitt"
+print("The given value is type/class:", type(c))
+
+d = False
+print("The given value is type/class:", type(d))
+
+
+
