d72bf1f284e71c489ef167216cf63c2a817334a3
ManittK <manitt.kedia@gmail.com>
2025-12-25 20:41:30 +0530

    py9

---
+++ congrats-9.py
@@ -0,0 +1,6 @@
+message = "Congrats On your fIrst miSsion to the EaRTH - 616"
+print("This is the actual message ", message)
+message = message.lower()
+print("This is the lower version of the message raw ", message)
+message = message.upper()
+print("This is the higher version of the message raw ", message)
