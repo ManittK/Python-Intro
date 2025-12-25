5ebcad1815e81f3bf4c69a74ddd15fa86dd26427
ManittK <manitt.kedia@gmail.com>
2025-12-25 20:31:00 +0530

    py8

---
+++ Reverse-8.py
@@ -0,0 +1,4 @@
+name= str(input("Kindly without hesitation type in your name, or else:"))
+revText = name[::-1]
+name= revText
+print("the reverse of the string or whatever or whosever name you have given is kindly the following ", name)
