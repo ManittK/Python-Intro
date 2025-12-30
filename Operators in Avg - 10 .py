2fcf09615765c66a4d238a74cab48a1120d74e3a
ManittK <manitt.kedia@gmail.com>
2025-12-30 20:28:08 +0530

    py10

---
+++ Operators in Avg - 10 .py
@@ -0,0 +1,10 @@
+tree_1 = 98
+tree_2 = 94
+tree_3 = 41
+tree_4 = 96
+tree_5 = 11
+
+#Avg Height is equal to sum of height by no. of heights observed
+SumH = tree_1+tree_2+tree_3+tree_4+tree_5
+AvgH = SumH/5
+print("The average height of the trees were observed as given", AvgH)
