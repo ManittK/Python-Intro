aaa295e60fe953fcb66a1557201533743ff38cc4
ManittK <manitt.kedia@gmail.com>
2026-01-01 20:29:59 +0530

    py15

---
+++ compare - 15.py
@@ -0,0 +1,8 @@
+num_a = int(input("Enter a random number between infinite to infinite or else i'll use your phone as test subject:"))
+num_b = int(input("Same command enter another number(DISTINCT OR ELSE...) again but this time the threat is that if you don't i'll use your horrible grades as a way to give a number:"))
+if num_a > num_b:
+    print("OMG! num_a takes the lead in the race and is beating num_b like a ez race")
+elif num_b > num_a:
+    print("Num_b shows power with confidence a full combo blast on num_a cooking it to fresh solid win for num_b")
+else:
+    print("Well I told you to be distinct and now you will pay with both your phone and your grades muahhhhahah!")
