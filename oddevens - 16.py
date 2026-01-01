6a497eeb8cd841f20b995b29cff1357cda42cb5c
ManittK <manitt.kedia@gmail.com>
2026-01-01 20:38:15 +0530

    py16

---
+++ oddevens - 16.py
@@ -0,0 +1,5 @@
+num = int(input("Well I tried to tell you last time but you didn't listen so you are going to pay for it by the hands of the almighty Iron Man and his side kick the balloon man Jarvis but if you wanna be saved just type in a number or else darkness will flood your sight💀💀💀:"))
+if num % 2 == 0:
+    print("Ok, you are a number by 2 guy so that means you want both smack from Hulk and a fearfull sight by the Scarlet Witch, but hey atleast you followed my orders this time. BY THE WAY IT WAS EVEN")
+else:
+    print("Ok, you are not a number by 2 guy so that means you want neither smack from Hulk nor a fearfull sight by the Scarlet Witch, but hey atleast you followed my orders this time. But still i'll keep you in my radar. BY THE WAY IT WAS ODD")
