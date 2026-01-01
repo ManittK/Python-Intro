d72e13bf42335676f66d20035b54027410de8c26
ManittK <manitt.kedia@gmail.com>
2026-01-01 20:15:33 +0530

    py13

---
+++ Positivity - 13.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number from -ve infinity to +ve infinity and then i'll tell you if it is a +ve number:"))
+if num > 0:
+    print("Yep, you got it positively")
+else: 
+    print("You either negative or a zero hero")
