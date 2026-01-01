4db767a63eea66bdacdfa4d09983bdecc0757ef2
ManittK <manitt.kedia@gmail.com>
2026-01-01 20:54:15 +0530

    py17

---
+++ conditional - 17.py
@@ -0,0 +1,7 @@
+chch = str(input("Name an Avenger who is strong but arrogant , smart but illogical and loves a suit of armor around him. Buy this time you know it is Iron Man so the question gets harder what weapon made Tony Stark be Iron Man HINT: It is a weapon which was showcased by him then momentarily caused a territorial attack on him: "))
+if chch == "Jeriko":
+    print("Yo you are an Avenger now, but we should always pay respect to the alimighty Iron Man who lived to die for the universe and it also means you saw the IRON MAN movie. Good People")
+elif not chch == "Jeriko":
+    print("How dare you not know this the devil MAD TITAN himself will hunt you down atleast you could have written IDK instead of", chch,"But next time I see you do this I will be the one to pick you out of school")
+elif chch == "":
+    print("Ye I understand Watch Marvel on JioHotstar by scrolling down down down down to the section written Hotstar click it then slide 1 right and click marvel and Enjoy The MCU!")
